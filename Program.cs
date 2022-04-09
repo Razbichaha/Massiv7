9ca2a775b103712b802ace7d6fba8ce96e63e211
Сергей <seregabur@live.ru>
2022-04-09 12:36:08 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Massiv7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringText = "Дана строка с текстом, используя метод строки String.Split() " +
+                         "получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
+
+            string[] stringArray = stringText.Split(' ');
+
+            foreach(string str in stringArray)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
